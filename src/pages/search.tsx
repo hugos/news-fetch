01f3b2bbeae520420dae6a33b8ae327a91b29634
Hugo <hugogando@gmail.com>
2023-06-22 12:14:39 +0100

    Developing Search form

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,7 +1,36 @@
+import { FormEvent, useState } from 'react'
+import { NewsArticle } from '../../models/NewsArticle';
+import { Form, Button } from 'react-bootstrap'
+
 const SearchNewsPage = () => {
+  // Search Results can either be an array of NewsArticle or Null. 
+  const [ searchResults, setSearchResults ] = useState<NewsArticle[] | null>(null);
+  const [ searchResultsIsLoading, setSearchResultsIsLoading ] = useState(false);
+  const [ searchResultsIsLoadingIsError, setSearchResultsIsLoadingIsError ] = useState(false);
+
+  // When submitting the form:
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault(); // stop from refreshing
+    const formData = new FormData(e.target as HTMLFormElement);
+    const searchQuery = formData.get("searchQuery")?.toString().trim();
+
+    if (searchQuery) {
+      alert(searchQuery)
+    }
+  }
+
+
   return (
     <main>
       <h1>Search News</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className='mb-3' controlId='search-input'>
+            <Form.Label>Search Query</Form.Label>
+            <Form.Control name="searchQuery" placeholder="E.g. politics, sports, ..."/>
+        </Form.Group>
+        <Button type="submit" className='mb-3' disabled={searchResultsIsLoading}>Search</Button>
+      </Form>
     </main>
   );
 };
